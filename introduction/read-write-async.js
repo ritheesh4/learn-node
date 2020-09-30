d2dcd873e915d9c4556d856b012570792fd1a286
Ritheesh <ritheesh.4@gmail.com>
2020-09-30 15:43:55 +0530

    asyn fucntion with call backs performed

--- introduction/read-write-async.js
+++ introduction/read-write-async.js
@@ -2,8 +2,14 @@
 
 const fs = require('fs');
 
-fs.readFile('./input.txt', 'utf-8', (err, data) => {
-    console.log(data);
+fs.readFile('./test.txt', 'utf-8', (err, data1) => {
+    console.log(data1);
+    fs.readFile(`./${data1}.txt`,'utf-8', (err, data2) =>{
+        console.log(data2);
+        fs.writeFile('./final.txt', `${data1}\n${data2}`, 'utf-8', err => {
+            console.log('Your file has been written');
+        })
+    })
 });
 
 console.log("Will read file");
