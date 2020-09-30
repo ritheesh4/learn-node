f6896e5599501e4185cd976194e0af9cc8dc2084
Ritheesh <ritheesh.4@gmail.com>
2020-09-30 16:21:33 +0530

    basic routing and request response implemented

--- first-server/index.js
+++ first-server/index.js
@@ -1,11 +1,25 @@
 const http = require('http');
+const url = require('url');
 
 //SERVER
-const server = http.createServer((req, res)=>{
-    console.log(req)
-    res.end('Hello from the server!');
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === '/products') {
+        res.end('This is the product');
+    } else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-own-header': 'hello-wrld'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
-server.listen(8000, '127.0.0.1', () =>{
+server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to request on port 8000');
 });
+
