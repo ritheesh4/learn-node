e5c39d7f7f76b29f298d9e93703fa16fca345c4d
Ritheesh <ritheesh.4@gmail.com>
2020-10-01 11:57:07 +0530

    Basic api response implemented

--- first-server/index.js
+++ first-server/index.js
@@ -1,18 +1,26 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 //SERVER
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
         res.end("This is the OVERVIEW");
-    } else if (pathName === '/products') {
+    } else if (pathName === '/api') {
+     res.writeHead(200, {'Content-type' : 'application/json'});
+     res.end(data)
+    }
+    else if (pathName === '/products') {
         res.end('This is the product');
     } else {
         res.writeHead(404, {
-            'Content-type' : 'text/html',
+            'Content-type': 'text/html',
             'my-own-header': 'hello-wrld'
         });
         res.end('<h1>Page not found!</h1>');
