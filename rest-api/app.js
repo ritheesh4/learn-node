7bfb1bd1e141c01a129a0044ef243a7c0f986227
Ritheesh <ritheesh.4@gmail.com>
2020-12-18 12:37:45 +0530

    refactoring until routes completed

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,41 +1,51 @@
 const fs = require("fs");
 const express = require("express");
+const { create } = require("domain");
 
 const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//     res
-//         .status(200)
-//         .json({ message: "Hello from the server side!", app: "Natours" });
-// });
-
-// app.post('/', (req, res) => {
-//     res.send("You can post to this endpoint....");
-// })
+res.send("You can post to this endpoint....");
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: "success",
         data: {
             tours,
         },
     });
-});
+}
 
-app.post("/api/v1/tours", (req, res) => {
-    //   console.log(req.body);
+const getTour = (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+}
+
+const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body)
 
     tours.push(newTour);
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours) , err => {
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'sucess',
             data: {
@@ -43,9 +53,14 @@ app.post("/api/v1/tours", (req, res) => {
             }
         })
     })
+}
 
-    res.send("Done");
-});
+// app.get("/api/v1/tours", getAllTours);
+// app.get("/api/v1/tours/:id", getTour);
+// app.post("/api/v1/tours", createTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour)
+app.route("/api/v1/tours/:id").get(getTour)
 
 const port = 3000;
 app.listen(port, () => {
